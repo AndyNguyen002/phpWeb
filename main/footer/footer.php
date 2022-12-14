c35375c7bc1d2a5d98ed95555f59c14cd18a5cbd
AndyNguyen002 <ngocdungokla@gmail.com>
2022-12-14 10:57:55 +0700

    footer

--- main/footer/footer.php
+++ main/footer/footer.php
@@ -1,42 +1,57 @@
-<footer>
-        <div class="container py-4">
-            <div class="row">
-                <div class="col-md-3 col-xs-6">
-                    <div class="gioithieu">
-                        <h3 class="header text-uppercase font-weight-bold">Về Computer Store</h3>
-                        <a href="#">Giới thiệu về Computer Store</a>
-                        <a href="#">Tuyển dụng</a>
-                        <div class="fb-like" data-href="https://www.facebook.com/DealBook-110745443947730/"
-                            data-width="300px" data-layout="button" data-action="like" data-size="small"
-                            data-share="true"></div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-6">
-                    <div class="hotrokh">
-                        <h3 class="header text-uppercase font-weight-bold">HỖ TRỢ KHÁCH HÀNG</h3>
-                        <a href="#">Hướng dẫn đặt hàng</a>
-                        <a href="#">Phương thức thanh toán</a>
-                        <a href="#">Phương thức vận chuyển</a>
-                        <a href="#">Chính sách đổi trả</a>
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-6">
-                    <div class="lienket">
-                        <h3 class="header text-uppercase font-weight-bold">HỢP TÁC VÀ LIÊN KẾT</h3>
-                        <img src="images/dichvu/dang-ky-bo-cong-thuong.png" alt="dang-ky-bo-cong-thuong">
-                    </div>
-                </div>
-                <div class="col-md-3 col-xs-6">
-                    <div class="ptthanhtoan">
-                        <h3 class="header text-uppercase font-weight-bold">Phương thức thanh toán</h3>
-                        <img src="images/dichvu/visa-payment.jpg" alt="visa-payment">
-                        <img src="images/dichvu/master-card-payment.jpg" alt="master-card-payment">
-                        <img src="images/dichvu/jcb-payment.jpg" alt="jcb-payment">
-                        <img src="images/dichvu/atm-payment.jpg" alt="atm-payment">
-                        <img src="images/dichvu/cod-payment.jpg" alt="cod-payment">
-                        <img src="images/dichvu/payoo-payment.jpg" alt="payoo-payment">
-                    </div>
-                </div>
+<style>
+    <?php include 'footer.css'; ?>
+</style>
+<!-- Footer -->
+<div class="Footer">
+        <div class="f-content">
+          <div class="col1">
+            <ul>
+              <li>Tích điểm Quà tặng VIP</li>
+              <li>Lịch sử mua hàng</li>
+              <li>Tìm hiểu về mua trả góp</li>
+              <li>Chính sách bảo hành</li>
+            </ul>
+          </div>
+          <div class="col1">
+            <ul>
+              <li>Giới thiệu công ty (MWG.vn)</li>
+              <li>Tuyển dụng</li>
+              <li>Gửi góp ý, khiếu nại</li>
+              <li>Tìm siêu thị (3.345 shop)</li>
+            </ul>
+          </div>
+          <div class="col1">
+            <ul>
+              <li>Tổng đài hỗ trợ (Miễn phí gọi)</li>
+              <li>Gọi mua: 1800.1060 (7:30 - 22:00)</li>
+              <li>Kỹ thuật: 1800.1763 (7:30 - 22:00)</li>
+              <li>Khiếu nại: 1800.1062 (8:00 - 21:30)</li>
+              <li>Bảo hành: 1800.1064 (8:00 - 21:00)</li>
+            </ul>
+          </div>
+          <div class="col2">
+            <div class="contact">
+              <img src="Icon/fblogo.png" alt=""> <span>3775.2k Fan</span>
+              <img src="Icon/ytlogo.png" alt=""> <span>851k Đăng ký</span>
+              <img src="Icon/icon_zalo-removebg-preview.png" alt=""> <span>Zalo TGDĐ</span>
             </div>
+            <div class="certificate">
+              <img src="Icon/DaThongBao.png" alt="">
+              <img src="Icon/RedOne.png" alt="">
+              <img src="Icon/DMCA.png" alt="">
+              <img src="Icon/handle_cert.png" alt="" style="width: 85px;height: 39px;margin-top: 10px;">
+            </div>
+            <div class="website">
+              <span style="margin-left:10px ;">Website cùng tập đoàn</span> <br>
+              <img src="Icon/topzone.png" alt="">
+              <img src="Icon/DIenMayXanh.png" alt="">
+              <img src="Icon/BachHoaXanh.png" alt="">
+              <img src="Icon/AVAFashion.png" alt="">
+              <img src="Icon/AVAJI.png" alt="">
+              <img src="Icon/AVAKid.png" alt="">
+              <img src="Icon/AVASport.png" alt="">
+            </div>
+          </div>
         </div>
-    </footer>
+      </div>
+      <!-- end footer -->
